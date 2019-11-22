97be1f5708ef0367edf0b43bac3520fe838b9cbb
Raul Jiminian <RJiminian@iBooks-MacBook-Pro.local>
2019-11-21 23:14:36 -0500

    Logic completed

--- main.js
+++ main.js
@@ -1,46 +1,365 @@
 const DataScienceAcronyms = [
   {
     id: 1,
-    title: "LINEI",
-    content_one: "L: Linear relationship between X and Y",
-    content_two: "I: Independence of residuals (errors)",
-    content_three: "N: Normality of residuals (errors)",
-    content_four: "E: Equality of variances",
-    content_five: "I: Independence of observations",
-    content_six: "",
-    content_seven: "",
-    content_eight: "",
-    content_nine: "",
-    content_ten: ""
+    title: "ANOVA",
+    content: [
+      "Analysis of Variance"
+    ]
   },
   {
     id: 2,
-    title: "LINEI",
-    content_one: "L: Linear relationship between X and Y",
-    content_two: "I: Independence of residuals (errors)",
-    content_three: "N: Normality of residuals (errors)",
-    content_four: "E: Equality of variances",
-    content_five: "I: Independence of observations",
-    content_six: "",
-    content_seven: "",
-    content_eight: "",
-    content_nine: "",
-    content_ten: ""
+    title: "AUC",
+    content: [
+      "Area Under the Curve"
+    ]
   },
   {
     id: 3,
-    title: "LINEI",
-    content_one: "L: Linear relationship between X and Y",
-    content_two: "I: Independence of residuals (errors)",
-    content_three: "N: Normality of residuals (errors)",
-    content_four: "E: Equality of variances",
-    content_five: "I: Independence of observations",
-    content_six: "",
-    content_seven: "",
-    content_eight: "",
-    content_nine: "",
-    content_ten: ""
+    title: "AUROC",
+    content: [
+      "Area Under Receiver Operating Characteristic"
+    ]
   },
+  {
+    id: 4,
+    title: "CART",
+    content: [
+      "Classification and Regression Trees"
+    ]
+  },
+  {
+    id: 5,
+    title: "CV",
+    content: [
+      "Cross Validation"
+    ]
+  },
+  {
+    id: 6,
+    title: "CNN",
+    content: [
+      "Convolutional Neural Network"
+    ]
+  },
+  {
+    id: 7,
+    title: "DBScan",
+    content: [
+      "Density-Based Spatial Clustering of Applications with Noise"
+    ]
+  },
+  {
+    id: 8,
+    title: "DNN",
+    content: [
+      "Deep Neural Network"
+    ]
+  },
+  {
+    id: 9,
+    title: "EDA",
+    content: [
+      "Exploratory Data Analysis"
+    ]
+  },
+  {
+    id: 10,
+    title: "ETL",
+    content: [
+      "Extract, Trasform, Load"
+    ]
+  },
+  {
+    id: 11,
+    title: "GAN",
+    content: [
+      "Generative Adversarial Network"
+    ]
+  },
+  {
+    id: 12,
+    title: "GBM",
+    content: [
+      "Gradient Boosting Machine"
+    ]
+  },
+  {
+    id: 13,
+    title: "GLM",
+    content: [
+      "Generalized Linear Model"
+    ]
+  },
+  {
+    id: 14,
+    title: "GRU",
+    content: [
+      "Gated Recurrent Unit"
+    ]
+  },
+  {
+    id: 15,
+    title: "HMM",
+    content: [
+      "Hidden Marcov Model"
+    ]
+  },
+  {
+    id: 16,
+    title: "IQR",
+    content: [
+      "Interquartile Range"
+    ]
+  },
+  {
+    id: 17,
+    title: "JSON",
+    content: [
+      "JavaScript Object Notation"
+    ]
+  },
+  {
+    id: 18,
+    title: "kNN",
+    content: [
+      "k-Nearest Neighbors"
+    ]
+  },
+  {
+    id: 19,
+    title: "LDA",
+    content: [
+      "Latent Dirichlet Allocation or Linear Discriminant Analysis"
+    ]
+  },
+  {
+    id: 20,
+    title: "LINEI",
+    content: [
+      "L: Linear relationship between X and Y",
+      "I: Independence of residuals (errors)",
+      "N: Normality of residuals (errors)",
+      "E: Equality of variances",
+      "I: Independence of observations"
+    ]
+  },
+  {
+    id: 21,
+    title: "LOOCV",
+    content: [
+      "Leave-One-Out Cross-Validation"
+    ]
+  },
+  {
+    id: 22,
+    title: "LSA",
+    content: [
+      "Latent Semantic Allocation"
+    ]
+  },
+  {
+    id: 23,
+    title: "LTSM",
+    content: [
+      "Long Term Short Memory"
+    ]
+  },
+  {
+    id: 24,
+    title: "MAE",
+    content: [
+      "Mean Absolute Error"
+    ]
+  },
+  {
+    id: 25,
+    title: "MCMC",
+    content: [
+      "Markov Chain Monte Carlo"
+    ]
+  },
+  {
+    id: 26,
+    title: "MedAE",
+    content: [
+      "Median Absolute Error"
+    ]
+  },
+  {
+    id: 27,
+    title: "ML",
+    content: [
+      "Machine Learning"
+    ]
+  },
+  {
+    id: 28,
+    title: "MLaaS",
+    content: [
+      "Machine Learning-as-a-Service"
+    ]
+  },
+  {
+    id: 29,
+    title: "MLR",
+    content: [
+      "Multiple Linear Regression"
+    ]
+  },
+  {
+    id: 30,
+    title: "MSE",
+    content: [
+      "Mean Squared Error"
+    ]
+  },
+  {
+    id: 31,
+    title: "NLDR",
+    content: [
+      "Non-Linear Dimensionality Reduction"
+    ]
+  },
+  {
+    id: 32,
+    title: "NaN",
+    content: [
+      "Not a Number"
+    ]
+  },
+  {
+    id: 33,
+    title: "NLP",
+    content: [
+      "Natural Language Processing"
+    ]
+  },
+  {
+    id: 34,
+    title: "OLAP",
+    content: [
+      "Online Analytical Processing"
+    ]
+  },
+  {
+    id: 35,
+    title: "OLSR",
+    content: [
+      "Ordinary Least Squares Regression"
+    ]
+  },
+  {
+    id: 36,
+    title: "OLTP",
+    content: [
+      "Online Transactional Processing"
+    ]
+  },
+  {
+    id: 37,
+    title: "PCA",
+    content: [
+      "Principal Component Analysis"
+    ]
+  },
+  {
+    id: 38,
+    title: "PLSA",
+    content: [
+      "Probabilistic Latent Semantic Allocation"
+    ]
+  },
+  {
+    id: 39,
+    title: "R2",
+    content: [
+      "R-squared"
+    ]
+  },
+  {
+    id: 40,
+    title: "RF",
+    content: [
+      "Random Forest"
+    ]
+  },
+  {
+    id: 41,
+    title: "RFE",
+    content: [
+      "Recursive Feature Elimination"
+    ]
+  },
+  {
+    id: 42,
+    title: "RMS",
+    content: [
+      "Root Mean Squared Error"
+    ]
+  },
+  {
+    id: 43,
+    title: "RNN",
+    content: [
+      "Recurrent Neural Network"
+    ]
+  },
+  {
+    id: 44,
+    title: "ROC",
+    content: [
+      "Receiver Operating Characteristic"
+    ]
+  },
+  {
+    id: 45,
+    title: "SLR",
+    content: [
+      "Single Linear Regression"
+    ]
+  },
+  {
+    id: 46,
+    title: "SMOTE",
+    content: [
+      "Synthetic Minority Over-Sampling Technique"
+    ]
+  },
+  {
+    id: 47,
+    title: "SQL",
+    content: [
+      "Structure Query Language"
+    ]
+  },
+  {
+    id: 48,
+    title: "SVM",
+    content: [
+      "Support Vector Machine"
+    ]
+  },
+  {
+    id: 49,
+    title: "TF - IDF",
+    content: [
+      "Term Frequency, Inverse Document Frequency"
+    ]
+  },
+  {
+    id: 50,
+    title: "t - SNE",
+    content: [
+      "t-Distributed Stochastic Neighbor Embedding"
+    ]
+  },
+  {
+    id: 51,
+    title: "YARN",
+    content: [
+      "Yet Another Resource Manager"
+    ]
+  }
 ];
 
 const sectionOutput = document.querySelector(".container");
@@ -50,7 +369,6 @@ let dataScienceList = () => {
 
   for (let i = 0; i < DataScienceAcronyms.length; i++) {
     let currentObj = DataScienceAcronyms[i];
-    console.log(currentObj)
     let cardDiv = document.createElement('div');
     cardDiv.className = `card`;
     let faceOne = document.createElement('div');
@@ -58,21 +376,17 @@ let dataScienceList = () => {
     let infoOne = document.createElement('div');
     infoOne.className = `content info1`;
 
-    let line;
-    for (let j = 2; j < 12; j++) {
-
-      if (currentObj[j] !== '') {
-        line = document.createElement('p');
-        line.textContent = `${currentObj[j]}`
-      }
-    }
+    currentObj.content.map(item => {
+      var line = document.createElement('p');
+      line.textContent = item
+      infoOne.append(line);
+    })
 
     let faceTwo = document.createElement('div');
     faceTwo.className = `face face2 info2`;
     let title = document.createElement('h2');
-    title.textContent = `${DataScienceAcronyms.title}`
+    title.textContent = `${currentObj.title}`
 
-    infoOne.append(line);
     faceOne.append(infoOne);
     faceTwo.append(title);
     cardDiv.append(faceOne);
@@ -82,10 +396,3 @@ let dataScienceList = () => {
 };
 
 dataScienceList()
-// $(document).ready();
-
-// $(window).on("load", dataScienceList())
-
-// window.onload = () => {
-//   dataScienceList()
-// }
